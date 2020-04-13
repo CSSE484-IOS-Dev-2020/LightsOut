98815f27c0bfa8bb51a1cc3019caac4fac97f26e
Yuhong Xu <yxu0901@gmail.com>
2020-04-13 02:06:56 -0400

    Initial Commit

---
+++ LightsOut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightsOut
+//
+//  Created by Yuhong Xu on 4/13/20.
+//  Copyright © 2020 Yuhong Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
